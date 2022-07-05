876e60e2c77cb92993e79c9b7c6a499e51f7fbce
Kauan Lopes <>
2022-07-05 18:47:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+print("*****************************")
+print("Welcome to the guessing game!")
+print("*****************************")
+
+secret_number = 42
+
+guess = -1
+
+#Jeito de não deixar o codigo dar erro após inserção de caracteres.
+
+while guess <0 :
+
+        try:
+                guess = int(input("Enter your number: "))
+                print ("You have choose", guess, sep = ' ')
+                break
+        except:
+                print("Invalid value, please just numbers!")
+                continue
+                
+if (secret_number == guess):
+        print ("Right Number")
+else:
+
+        print("Wrong Number")
+
+
+
+
+        
+                  
