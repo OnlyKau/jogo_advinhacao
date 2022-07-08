b4640b75e6d1d03455de064e24d1769f9b07047d
Kauan Lopes <98911353+OnlyKau@users.noreply.github.com>
2022-07-08 21:10:04 +0000

    Version 0.2

--- main.py
+++ main.py
@@ -1,41 +1,64 @@
+import random 
+
 print("*****************************")
 print("Welcome to the guessing game!")
 print("*****************************")
 
-secret_number = 42
-
-guess = -1
+secret_number = random.randint(1,10)
+print (secret_number)
+guess = ()
+total_try = 3
+round = 1
 
 #Jeito de não deixar o codigo dar erro após inserção de caracteres.
 
-while guess <0 :
+'''print ("Try ",t) 
+while (total_try > 0):
+  
+  if guess <0 :
 
-  try:
-    guess = int(input("Enter your number: "))
-    print ("You have choose", guess, sep = ' ')
-    break
-  except:
-    print("Invalid value, please just numbers!")
-    continue
+    try:
+      guess = int(input("Enter your number: "))
+      print ("You have choose", guess, sep = ' ')
+      break
+    except:
+      print("Invalid value, please just numbers!")
+      continue
+'''
+while (total_try > 0):
 
-right =  secret_number == guess  
+  print ("Try {} of 3".format(round))
+  
+  guess = int(input("Type a number: "))
+  
+  print("you typed: ", guess)
 
-high  = guess > secret_number
+  
+  right =  secret_number == guess  
 
-low   = guess < secret_number
+  high  = guess > secret_number
+
+  low   = guess < secret_number
                 
-if (right):
+  if (right):
 
-  print ("Right Number")
+    print ("Right Number")
 
-else:
+  else:
         
-  if(high):
-    print("Wrong Number, You chosse a higher number than secret number.")
+    if(high):
+      print("Wrong Number, You chosse a higher number than secret number.")
         
-  if(low):
-    print("Wrong Number, you choose a lower number than secret number.")
+    elif(low):
+      print("Wrong Number, you choose a lower number than secret number.")
+  
+  if (guess == secret_number):
+    break
 
+
+ 
+  total_try = total_try - 1
+  round = round + 1
 
 print("End Game")          
  
